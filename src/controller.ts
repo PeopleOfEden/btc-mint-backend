a90533cc83db59d408cff706f8e8adc9df847c26
Steven Enamakel <enamakel@mahadao.com>
2023-03-27 03:25:58 +0300

    added mint wallet details

--- src/controller.ts
+++ src/controller.ts
@@ -15,6 +15,7 @@ export const getWinners = async (req: Request, res: Response) => {
 export const getProgress = async (req: Request, res: Response) => {
   const mintAmount = nconf.get("MINT_AMOUNT") || 0.03;
 
+  const mintWallet = nconf.get("MINT_WALLET");
   const mintWalletBalance = 0; // todo add a api to check wallet balance
 
   const totalMintAmount = nconf.get("MINT_SUPPLY_CAP") || 222;
@@ -24,5 +25,5 @@ export const getProgress = async (req: Request, res: Response) => {
       totalMintAmount) *
     100;
 
-  res.json({ success: true, progress });
+  res.json({ success: true, progress, mintWallet, mintWalletBalance });
 };
